941b3a73d2d8c2168992e63a72334263655c3061
kd2-80158 <chhimwalsaurabh@gmail.com>
2024-02-28 09:17:17 +0530

    mapShare file modified

--- Map/mapShare.java
+++ Map/mapShare.java
@@ -1,5 +1,18 @@
 import java.util.Map;
 
+/**
+ * Created by Saurabh Chhimwal on 28/02/2024.
+ * github.com/kd2-80158
+ * chhimwalsaurabh@gmail.com
+ * <p>
+ * Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value.
+ * In all cases remove the key "c", leaving the rest of the map unchanged.
+ * <p>
+ * mapShare({"b": "bbb", "c": "ccc", "a": "aaa"}) → {"b": "aaa", "a": "aaa"}
+ * mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+ * mapShare({"d": "hi", "c": "meh", "a": "aaa"}) → {"d": "hi", "b": "aaa", "a": "aaa"}
+ */
+
 public Map<String, String> mapShare(Map<String, String> map) {
   
   // if(map.containsKey("a") && map.containsKey("c"))
